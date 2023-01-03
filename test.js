060226595d39f772dd92baf8ba3fca97ebad5f92
Marcos Conceicao <marcos.conceicao@nubank.com.br>
2023-01-03 19:56:19 -0300

    test new

---
+++ test.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+axios.get('http://localhost:14240/api/ping')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+});
+
+axios.get('http://localhost:9000/version')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+});
